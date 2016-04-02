2d3b8bf8fe0a4aaf473bdbe2f78b64255fef8bb2
Hesowcharov <Hesowcharov@gmail.com>
2016-04-02 18:49:58 +0400

    override in method for require

--- src/main/scala/Neuro/Layer/InputLayer.scala
+++ src/main/scala/Neuro/Layer/InputLayer.scala
@@ -4,6 +4,11 @@ package Neuro.Layer
   * Created by HesowcharovU on 02.04.2016.
   */
 class InputLayer private (n: Int) extends Layer(n) {
+  override def in(xs: Array[Double]): Unit = {
+    require(dim == xs.length)
+    super.in(xs)
+  }
+
   def out: Array[Double] = {
     require(inp.isDefined)
     inp.get

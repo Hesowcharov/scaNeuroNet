efd760d1360b4457da00448d76e740172fae249a
Hesowcharov <Hesowcharov@gmail.com>
2016-04-02 13:52:38 +0400

    main test

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,6 +1,13 @@
 import Neuro._
 
 object MainApp extends App {
-  val net = Network (5) <:> 3 <:> 7 <:> 12
-  println("It was done!")
+  var net = Network (1) <:> 1
+  net = net.!! (Array(10), Array(0))
+    .!!(Array(15), Array(0))
+    .!!(Array(-2), Array(0))
+    .!!(Array(17), Array(1))
+    .!!(Array(125), Array(1))
+    .!!(Array(56), Array(1))
+  val outp = net.activate(Array(-1111))
+  println(outp.mkString(" "))
 }

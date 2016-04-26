962363c7f246dc06760366a00d7630432be01137
Hesowcharov <Hesowcharov@gmail.com>
2016-04-26 18:01:14 +0400

    Added teacher class with scale methods

---
+++ src/main/scala/Neuro/Teacher.scala
@@ -0,0 +1,20 @@
+package Neuro
+
+/**
+  * Created by HesowcharovU on 07.04.2016.
+  */
+class Teacher (min: Double, max: Double, left: Double, right: Double) {
+  def changeScale(value: Double) : Double = (value - min) * (right - left) / (max - min) + left
+  def resetScale(value: Double): Double = (max - min) * (value - left) / (right - left) + min
+  def teach(net: Network, samples: (Array[Double], Array[Double])): Network = {
+    val (inSample, outSample) = samples
+    val fixInSample = inSample.map { changeScale }
+    val fixOutSample = outSample.map { changeScale }
+    net.!!(fixInSample, fixOutSample)
+  }
+}
+
+object Teacher {
+  def changeScale(value: Double, min: Double, max: Double, left: Double, right: Double) : Double =
+    (value - min) * (right - left) / (max - min) + left
+}
